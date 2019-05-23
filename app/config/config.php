211bb36075936707830186081291a7066c0c18d6
Goran Milic <gmilic@gmail.com>
2019-05-23 16:43:15 +0200

    19-05-23 17:00

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,13 @@
 <?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'mvc');
+define('DB_PASS', '123456');
+define('DB_NAME', 'mvc');
+
+
+
+
 // App root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
